2dec789ca8a730deee631ec213b207d3f2fb5a49
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2022-06-27 09:57:50 +0700

    seperate env

--- packages/examples/simple-tele-bot/src/environments/environment.prod.porichain.ts
+++ packages/examples/simple-tele-bot/src/environments/environment.prod.porichain.ts
@@ -9,13 +9,13 @@ export const environment = {
     __dirname,
     '../../../../archived/repo/.web3session'
   ),
-  botMemoryPath: path.join(__dirname, '../../../../archived/repo/.botMem'),
+  botMemoryPath: path.join(__dirname, '../../../../archived/repo/.botMemDante'),
   mongodbDataStoreUri: process.env.MONGODB_DATA_STORE_URI,
   mongodbDataStoreSSLCer: path.join(
     __dirname,
     '../../../../archived/repo/mongodb.pem'
   ),
-  aesKeyPath: path.join(__dirname, '../../../../archived/repo/.aesKey'),
+  aesKeyPath: path.join(__dirname, '../../../../archived/repo/.aesKeyDante'),
   createdBlock: 7643,
   production: true,
 };
