15122dbb2af1d8b2c9ef59f7c4d45d7d1e37610f
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2023-02-10 09:08:29 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,8 @@ ENV TZ="Asia/Saigon"
 
 WORKDIR /app
 
+COPY ./archived/repo ./archived/repo
 COPY ./dist/packages/examples/simple-tele-bot ./dist/packages/examples/simple-tele-bot
 RUN cd ./dist/packages/examples/simple-tele-bot && npm install --production
-COPY ./archived/repo ./archived/repo
 
 CMD [ "node", "./dist/packages/examples/simple-tele-bot/main.js" ]
--- packages/pori-actions/src/lib/auto/autoPlayWorkflow.ts
+++ packages/pori-actions/src/lib/auto/autoPlayWorkflow.ts
@@ -22,7 +22,7 @@ export { autoMonitorMarketItemPrices } from './autoMarketMonitor';
 export { autoKeepAliveBot } from './autoKeepAliveBot';
 
 export const ESB_P_THRESHOLD_KEEP_BIG_REWARD = 15;
-const MAX_PORI_ENGAGED_MISSION = 999;
+const MAX_PORI_ENGAGED_MISSION = 9999;
 const SBATTLE_BEFORE_END_MS = 30 * 60 * 1000; // 30 mins
 const ONE_HOUR_IN_MS = 1 * 60 * 60 * 1000; // 1 hour
 
